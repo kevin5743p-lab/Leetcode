3c90172d1c9723366080e20ff61668c32eed472d
kevin5743p-lab <kevin5743p@gmail.com>
2026-09-25 12:39:05 +0200

    Time: 16 ms (92.88%), Space: 53.5 MB (33.7%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode | None) -> bool:
+        pointer = head
+        arr = list()
+        while pointer:
+            arr.append(pointer.val)
+            pointer = pointer.next
+        lst = arr.copy()
+        arr.reverse()
+        if arr == lst:
+            return True
+        return False
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
